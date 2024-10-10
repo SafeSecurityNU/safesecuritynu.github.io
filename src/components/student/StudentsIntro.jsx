f282105e540ca98f59718c5b4a9d6dbcdbb3e31c
ksyun360 <ksyun360@gmail.com>
2024-10-09 20:16:00 -0500

    Update weekly meeting time

--- src/components/student/StudentsIntro.jsx
+++ src/components/student/StudentsIntro.jsx
@@ -10,7 +10,7 @@ export default function StudentsIntro() {
                 Interested in increasing access to understandable cybersecurity information in your communities? Passionate about discussing historical and current cybersecurity threats, mitigation techniques, and best practices? Simply eager to learn more about and engage with cybersecurity? If so, you should join Safe Security!
             </p>
             <p className="mt-10 text-lg italic font-bold text-center text-neutral-200 max-w-4xl">
-                The easiest way to get involved is join us at an upcoming meeting! For fall quarter 2024, Safe Security is meeting <span className="bg-gradient-to-r from-blue-300 to-purple-800 text-transparent bg-clip-text">Wednesdays from 7-8 PM in Tech L160.</span> 
+                The easiest way to get involved is join us at an upcoming meeting! For more information, sign up for our mailing list!
             </p>
         </div>
     )
